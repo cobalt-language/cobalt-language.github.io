805c4034ac4da427f296baf42827e1d8a01da530
treemcgee42 <varun.malladi@gmail.com>
2023-12-03 18:19:47 -0500

    basic blog structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cobalt-language.github.io',
-  base: '/cobalt-language.github.io',
+  site: "https://cobalt-language.github.io",
 });
